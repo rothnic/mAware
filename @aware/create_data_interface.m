ebdea5a0b5e505f4c1a42c6f6825fab6c43821d5
Nick Roth <nlr06886@gmail.com>
2014-08-09 12:16:17 -0500

    Custom grid working for both horizontal and vertical directions.

--- @aware/create_data_interface.m
+++ @aware/create_data_interface.m
@@ -65,6 +65,7 @@ gui.Window = figure( ...
 % Set default panel color
 uiextras.set( gui.Window, 'DefaultBoxPanelTitleColor', [0.7 1.0 0.7] );
 
+%% Setup Menus
 % + File menu
 gui.FileMenu = uimenu( gui.Window, 'Label', 'File' );
 uimenu( gui.FileMenu, 'Label', 'Exit', 'Callback', @onExit );
@@ -79,27 +80,41 @@ end
 helpMenu = uimenu( gui.Window, 'Label', 'Help' );
 uimenu( helpMenu, 'Label', 'Documentation', 'Callback', @onHelp );
 
-
 % Arrange the main interface
 mainLayout = uiextras.HBoxFlex( 'Parent', gui.Window, 'Spacing', 3 );
 
-% + Create the panels
+%% + Create the panels
 controlPanel1 = uiextras.BoxPanel( ...
     'Parent', mainLayout, ...
     'Title', 'Select a demo:' );
 controlPanel2 = uiextras.BoxPanel( ...
     'Parent', mainLayout, ...
     'Title', 'Select a demo:' );
-gui.ViewPanel = uiextras.BoxPanel( ...
-    'Parent', mainLayout, ...
+gui.ViewPanelVert = uiextras.VBoxFlex( 'Parent', mainLayout, ...
+    'Padding', 3, 'Spacing', 3 );
+gui.NewVertViewButton = uicontrol('Parent', gui.ViewPanelVert, ...
+    'String', '+', 'Callback', @addVertView);
+
+
+% Each row is a HBox
+gui.ViewPanel = uiextras.HBoxFlex( ...
+    'Parent', gui.ViewPanelVert );
+% Each row has a button to add more views
+gui.NewHorzViewButton = uicontrol('Parent', gui.ViewPanel, ...
+    'String', '+', 'Callback', @addHView);
+% Each view requires a box panel to sit in
+gui.viewArea = uiextras.BoxPanel( ...
+    'Parent', gui.ViewPanel, ...
     'Title', 'Viewing: ???', ...
     'HelpFcn', @onDemoHelp );
+% Set Button and initial View sizes
+set( gui.ViewPanel, 'Sizes', [15 -1]  );
+set( gui.ViewPanelVert, 'Sizes', [15, -1]  );
 
-% + Adjust the main layout
+%% + Adjust the main layout
 set( mainLayout, 'Sizes', [-1 -2 -3]  );
 
-
-% + Create the controls
+%% + Create the controls
 controlLayout1 = uiextras.VBox( 'Parent', controlPanel1, ...
     'Padding', 3, 'Spacing', 3 );
 controlLayout2 = uiextras.VBox( 'Parent', controlPanel2, ...
@@ -115,7 +130,7 @@ gui.ListBox2 = uicontrol( 'Style', 'list', ...
     'Parent', controlLayout2, ...
     'String', demoList(:), ...
     'Value', 1, ...
-    'Callback', @onListSelection);
+    'Callback', @onList2Selection);
 gui.HelpButton = uicontrol( 'Style', 'PushButton', ...
     'Parent', controlLayout1, ...
     'String', 'Help for <demo>', ...
@@ -123,17 +138,33 @@ gui.HelpButton = uicontrol( 'Style', 'PushButton', ...
 set( controlLayout1, 'Sizes', [-1 28] ); % Make the list fill the space
 set( controlLayout2, 'Sizes', [-1] );
 
-% + Create the view
-p = gui.ViewPanel;
+%% + Create the plot view
+p = gui.viewArea;
 gui.ViewAxes = axes( 'Parent', p );
 
-    function onListSelection( source, ~)
-        disp(source);
-        ctrlPanel = uiextras.BoxPanel( ...
-        'Parent', mainLayout, ...
-        'Title', 'Select a demo:' );
+%% Callback Functions
+    function addVertView( source, ~)
+        
+        newHorzViewPanel = uiextras.HBoxFlex( ...
+            'Parent', gui.ViewPanelVert );
+        NewHorzViewButton = uicontrol('Parent', newHorzViewPanel, ...
+            'String', '+', 'Callback', @addHView);
+        newAxisContainer = uiextras.BoxPanel( ...
+            'Parent', newHorzViewPanel, ...
+            'Title', 'Select a demo:' );
+        newView = axes( 'Parent', newAxisContainer );
+        
+        set( newHorzViewPanel, 'Sizes', [15 -1]  );
     end
 
+    function addHView( source, ~)
+        srcObj = get(source);
+        parent = srcObj.Parent;
+        ctrlPanel = uiextras.BoxPanel( ...
+        'Parent', parent, ...
+        'Title', 'Select a demo:' );
+        newView = axes( 'Parent', ctrlPanel );
+    end
 
 end % createInterface
 
